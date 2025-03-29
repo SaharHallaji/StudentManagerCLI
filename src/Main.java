ecc484840e95899161ef9f8d3016d987ee4135d1
sahar <saharhallaji.dev@gmail.com>
2025-03-29 15:45:27 +0330

    feat(structure): base file which are common are added.

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import helpers.MenuHelper;
+import repositories.StudentRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                int chosenOption = Integer.parseInt(MenuHelper.showMenu());
+
+                switch (chosenOption) {
+                    case 1 -> addStudent();
+                    case 2 -> listStudents();
+                    case 3 -> updateStudent();
+                    case 4 -> removeStudent();
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Invalid option!");
+                }
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void addStudent() {
+/*        Map<String, String> studentData = MenuHelper.addStudentFields();
+        String id = StudentRepository.INSTANCE.addStudent(studentData);
+        System.out.println("\nStudent added successfully with ID: " + id);*/
+    }
+
+    private static void listStudents() {
+/*        List<Map<String, String>> students = StudentRepository.INSTANCE.getAllStudents();
+        if (students.isEmpty()) {
+            System.out.println("\nNo students found!");
+        } else {
+            System.out.println("\nList of students: ");
+            students.forEach(student -> {
+                System.out.println("ID : " + student.get("id"));
+                System.out.println("Name : " + student.get("firstName") + " " + student.get("lastName"));
+                System.out.println("Semester : " + student.get("semester"));
+                System.out.println("Degree : " + student.get("degree"));
+                System.out.println("---------------------------------------------");
+            });
+
+            System.out.println("\nEnd of the list.");
+        }*/
+    }
+
+    private static void updateStudent() {
+/*        String id = MenuHelper.getStudentId();
+
+        Optional<Map<String, String>> existingStudent = StudentRepository.INSTANCE.getStudent(id);
+        existingStudent.ifPresentOrElse(_ -> {
+
+                    System.out.println("\nUpdating student with ID: " + id);
+
+                    Map<String, String> updatedData = MenuHelper.addStudentFields();
+
+                    if( StudentRepository.INSTANCE.updateStudent(id, updatedData) ) {
+                        System.out.println("\nStudent updated successfully with ID: " + id);
+                    } else {
+                        System.out.println("\nStudent update failed!");
+                    }
+
+                },
+                () -> System.out.println("\nStudent not found!")
+        );*/
+    }
+
+    private static void removeStudent() {
+//        /*String id = MenuHelper.getStudentId();
+//
+//        Optional<Map<String, String>> existingStudent = StudentRepository.INSTANCE.getStudent(id);
+//
+//        existingStudent.ifPresentOrElse(_ -> {
+//            if (StudentRepository.INSTANCE.deleteStudent(id)){
+//                System.out.println("\nStudent deleted successfully!");
+//            } else {
+//                System.out.println("\nStudent delete failed!");
+//            }
+//        }, () -> System.out.println("\nStudent not found!"));*/
+    }
+
+}
